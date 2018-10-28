f781d563b78ea99d22692d92d299e23d966bb22f
Vikas Malviya <40855821+SunnyVikasMalviya@users.noreply.github.com>
2018-10-28 23:52:02 +0530

    Add files via upload

---
+++ GPR1.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>GetPostRequest</title>
+	</head>
+	<body>
+		<form action="GPR2.php" method="get">
+		First Name:<input type="text" name="fname"><br/>
+		Last Name:<input type="text" name="lname"><br/>
+		<input type="Submit">
+		</form>
+	</body>
+</html>
---
+++ GPR2.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>GetPostRequest2</title>
+	</head>
+	<body>
+		Hello
+		<?php
+			echo $_REQUEST["fname"];
+		?>
+		,your name has been added.
+	</body>
+</html>
